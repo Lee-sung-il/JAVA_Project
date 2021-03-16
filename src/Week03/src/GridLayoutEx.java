54c207e953b4ef46d7f6f9f9359b97d60caf12c9
LeeSungil <wj132612@naver.com>
2021-03-16 16:06:55 +0900

    commit

--- src/Week03/src/GridLayoutEx.java
+++ src/Week03/src/GridLayoutEx.java
@@ -3,23 +3,23 @@ package Week03.src;
 import javax.swing.*;
 import java.awt.*;
 
-public class FlowLayoutEx extends JFrame {
+public class GridLayoutEx extends JFrame {
     private JButton btn1 = new JButton("Btn1");
     private JButton btn2 = new JButton("Btn2");
     private JButton btn3 = new JButton("Btn3");
     private JButton btn4 = new JButton("Btn4");
     private JButton btn5 = new JButton("Btn5");
-    FlowLayoutEx() {
-        this.setTitle("FlowLayout");
+    GridLayoutEx() {
+        this.setTitle("GridLayout");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.formDesign();
         this.eventHandler();
-        this.setSize(300, 200);
+        this.setSize(300,200);
         this.setVisible(true);
     }
 
     private void formDesign() {
-        this.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        this.setLayout(new GridLayout(3,2,5,5));
         this.add(btn1);
         this.add(btn2);
         this.add(btn3);
@@ -32,7 +32,6 @@ public class FlowLayoutEx extends JFrame {
     }
 
     public static void main(String[] args) {
-        new FlowLayoutEx();
+        new GridLayoutEx();
     }
-
 }
